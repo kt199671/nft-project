3795da06a60e82312b81681ccacb6f59d9c995e6
kentaro <ktfox1996@gmail.com>
2022-02-13 16:00:38 +0900

    add about page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 import { Grid } from '@material-ui/core';
 import Header from './components/Header';
 import Content from './components/Content';
+import About from './components/About';
 
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
@@ -14,7 +19,16 @@ function App() {
       <Grid item container>
         <Grid sm={2} />
         <Grid xs={12} sm={8}>
-          <Content />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Content />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </Router>
         </Grid>
         <Grid sm={2} />
       </Grid>
