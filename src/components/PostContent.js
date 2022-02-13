4fcc8319034266cbf6181086e5ed02d7a0d8cb30
kentaro <ktfox1996@gmail.com>
2022-02-13 20:37:55 +0900

    add postContent

--- src/components/PostContent.js
+++ src/components/PostContent.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom'
+
+import { Grid } from '@material-ui/core'
+import BodyCard from './BodyCard'
+
+
+const cardContent = 
+    {
+        avatarUrl: "https://joeschmoe.io/api/v1/random",
+        imageUrl: "https://picsum.photos/150"
+    }
 
 function PostContent() {
     const { id } = useParams();
+    const [post, setPosts] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(res => {
+            setPosts(res.data)
+        })
+    }, [])
+
     return (
-        <div>
-            <h1>個別記事 {id}</h1>
-        </div>
+        <Grid container spacing={2}>
+            <Grid item xs={12} key={post.id}>
+                <BodyCard {...{...post, ...cardContent}} />
+            </Grid>
+        </Grid>
     )
 }
 
